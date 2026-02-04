dd819bcac3d4de2b61721076f6b9b4cf628724a1
Adithyakakumanu <kakumanuaditya5@gmail.com>
2026-02-04 11:22:12 +0530

    updateds

--- Python/pandas.py
+++ Python/pandas.py
@@ -1,25 +1,31 @@
-import numpy as np
-arr = np.array([10, 20, 30, 40])
-# print(arr) #
-# print(arr.dtype) #Shows datatype
-
 import pandas as pd
+pd.Series([1,2,3,4])
 data = {
     "id": [1, 2, 3, 4, 4],
     "name": ["Ram", "Sita", "Ravi", "Amit", "Amit"],
     "marks": [85, None, 78, 90, 90],
     "city": ["Delhi", "Mumbai", None, "Chennai", "Chennai"]
 }
-
+pd.read_csv("data.csv")
+pd.read_excel("data.xlsx")
 df = pd.DataFrame(data)
 #print(df)
 df.head()      # First 5 rows
 df.tail()      # Last 5 rows
 df.shape      # Rows, Columns
-df.columns     # Column names
+df.columns 
+df.describe()
+df.index    # Column names
 df.info()      # Datatypes & null info
 df.isnull()
+df.notnull()
 df.isnull().sum()
+df.fillna(0)
+df.fillna(method="ffill")
+df.fillna(method="bfill")
+df.dropna()
+df.dropna(subset=["col"])
+
 df_no_duplicates = df.drop_duplicates()
 df_unique_id = df.drop_duplicates(subset="id") #remove based on the column
 #print(df_no_duplicates)
@@ -32,3 +38,12 @@ df["marks"] = df["marks"].astype(int) #convert to another datatype
 df["date"] = pd.to_datetime("2024-01-01") #convert to datetime
 df.sort_values(by="total_marks", ascending=False)
 df["city"].value_counts()
+df["name"].str.upper()
+df["name"].str.lower()
+df["name"].str.strip()
+df["name"].str.contains("a")
+df["name"].str.replace("a", "A")
+df.groupby("class").mean()
+df.groupby("class").sum()
+df.groupby("class").count()
+df.groupby("class").agg(["min","max","mean"])
